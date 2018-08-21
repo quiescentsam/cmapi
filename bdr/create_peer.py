b804c06529d20bf43e5acb9c3e145c475c67d6d7
sam <sameerahmad@cloudera.com>
2018-08-21 16:06:05 -0500

    2018082116

--- bdr/create_peer.py
+++ bdr/create_peer.py
@@ -3,6 +3,57 @@
 from cm_api.api_client import ApiResource
 from cm_api.endpoints.types import *
 
+
+def retrieve_args():
+    """
+    Attempts to retrieve Cloudera Manager connection information from the environment.
+    If that fails, the information is parsed from the command line.
+    @rtype:  namespace
+    @return: The parsed arguments.
+    """
+
+    if all(env_var in os.environ for env_var in ("DEPLOYMENT_HOST_PORT",
+                                                 "CM_USERNAME", "CM_PASSWORD",
+                                                 "CLUSTER_NAME")):
+        write_to_stdout("Arguments detected in environment -- command line arguments being ignored.\n")
+        args = namedtuple("args", ["host", "port", "username", "password", "cluster", "use_tls"])
+
+        parsed_url = os.environ["DEPLOYMENT_HOST_PORT"].split(":")
+        args.host = parsed_url[0]
+        args.port = int(parsed_url[1])
+        args.username = os.environ["CM_USERNAME"]
+        args.password = os.environ["CM_PASSWORD"]
+        args.cluster = os.environ["CLUSTER_NAME"]
+        args.use_tls = False
+
+        return args
+    else:
+        return parse_args()
+
+def parse_args():
+    """
+    Parses host and cluster information from the given command line arguments.
+    @rtype:  namespace
+    @return: The parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="Configure an existing cluster",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('host', metavar='HOST', type=str, help="The Cloudera Manager host")
+    parser.add_argument('cluster', metavar='CLUSTER', type=str,
+                        help="The name of the cluster to kerberize")
+    parser.add_argument('--port', metavar='port', type=int, default=7180,
+                        help="Cloudera Manager's port.")
+    parser.add_argument('--username', metavar='USERNAME', type=str, default='admin',
+                        help="The username to log into Cloudera Manager with.")
+    parser.add_argument('--password', metavar='PASSWORD', type=str, default='admin',
+                        help="The password to log into Cloudera Manager with.")
+    parser.add_argument('--use-tls', action='store_true',
+                        help="Whether to use TLS to connect to Cloudera Manager.")
+    return parser.parse_args()
+
+
+
+
 TARGET_CM_HOST = "<destination_cluster>"
 SOURCE_CM_URL = "<source_cluster>:7180/"
 
