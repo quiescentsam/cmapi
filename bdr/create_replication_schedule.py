095b9ce5989c30e0993fea22a637ff7d81018969
sam <sameerahmad@cloudera.com>
2018-08-21 17:00:01 -0500

    2018082117

--- bdr/create_replication_schedule.py
+++ bdr/create_replication_schedule.py
@@ -9,7 +9,7 @@ SOURCE_CLUSTER_NAME='sameer-testspot'
 SOURCE_HDFS_NAME='CD-HDFS-VHPVExTo'
 TARGET_CLUSTER_NAME='sameer-testspot-dest'
 TARGET_HDFS_NAME='CD-HDFS-KukHKtDK'
-TARGET_YARN_SERVICE='YARN-1CD-YARN-rnMjblqZ'
+TARGET_YARN_SERVICE='CD-YARN-rnMjblqZ'
 TARGET_CM_HOST="18.205.59.216"
 
 api_root = ApiResource(TARGET_CM_HOST, username="admin", password="admin")
