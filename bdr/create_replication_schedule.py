73267f3594d9c287eba479607dfa6bd15644be26
sam <sameerahmad@cloudera.com>
2018-08-22 11:09:00 -0500

    2018082211

--- bdr/create_replication_schedule.py
+++ bdr/create_replication_schedule.py
@@ -8,15 +8,17 @@ def get_service_name():
     Take the cluster APiResource and get SERVICE name
     :return:
     """
-    TARGET_CM_HOST="18.205.59.216"
-    TARGET_CLUSTER_NAME='sameer-testspot-dest'
-    api_target = ApiResource(TARGET_CM_HOST, username="admin", password="admin")
     cluster = api_target.get_cluster(TARGET_CLUSTER_NAME)
     services = cluster.get_all_services()
     for service_name in services:
         if "HDFS" in service_name.name:
             return service_name.name
 
+
+
+TARGET_CM_HOST="18.205.59.216"
+TARGET_CLUSTER_NAME='sameer-testspot-dest'
+api_target = ApiResource(TARGET_CM_HOST, username="admin", password="admin")
 
 TARGET_HDFS_NAME = get_service_name()
 print TARGET_HDFS_NAME
