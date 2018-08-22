fc3e3fe31334d6fb794a33ee1528d03b5b78ecae
sam <sameerahmad@cloudera.com>
2018-08-22 13:54:01 -0500

    2018082213

--- bdr/README.md
+++ bdr/README.md
@@ -1,3 +1,55 @@
+#BDR automation
+
+##ADD Peer
+
+```(python27) ssiddiqui-MBP15:bdr ssiddiqui$ python add_peer.py -h
+usage: add_peer.py [-h] [-s HOST] [-p port] [-u USERNAME] [-pwd PASSWORD]
+                   [--use-tls] [--source_cm_url Source Cloudera Manager URL]
+                   [--source-user Source Cloudera Manager Username]
+                   [--source-password SOURCE_CM_PWD] [--peer-name PEER_NAME]
+
+Adding Source cluster as 'peer' in Destination Cloudera Manager
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -s HOST, --server HOST
+                        The Cloudera Manager host (default: None)
+  -p port, --port port  Cloudera Manager's port. (default: 7180)
+  -u USERNAME, --username USERNAME
+                        The username to log into Cloudera Manager with.
+                        (default: admin)
+  -pwd PASSWORD, --password PASSWORD
+                        The password to log into Cloudera Manager with.
+                        (default: admin)
+  --use-tls             Whether to use TLS to connect to Cloudera Manager.
+                        (default: False)
+  --source_cm_url Source Cloudera Manager URL
+                        Full CM URL of the source cluster ie.
+                        https://hostname:7183/ (default: None)
+  --source-user Source Cloudera Manager Username
+                        The username to log into Source Cloudera Manager with.
+                        (default: admin)
+  --source-password SOURCE_CM_PWD
+                        The password to log into Source Cloudera Manager with.
+                        (default: admin)
+  --peer-name PEER_NAME
+                        ALias Name to be created of the Source cluster
+                        (default: peer1)
+
+```
+
+
+
+
+
+
+
+
+
+
+
+
+
 class ApiHdfsReplicationResult(BaseApiObject):
   _ATTRIBUTES = {
     'progress'            : ROAttr(),
--- bdr/create_replication_schedule.py
+++ bdr/create_replication_schedule.py
@@ -2,6 +2,34 @@
 
 from cm_api.api_client import ApiResource
 from cm_api.endpoints.types import *
+import argparse
+
+def parse_args():
+    """
+    Parses host and cluster information from the given command line arguments.
+    @rtype:  namespace
+    @return: The parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="Adding Source cluster as 'peer' in Destination Cloudera Manager ",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-s', '--server', metavar='HOST', type=str, help="The Cloudera Manager host")
+    parser.add_argument('-p', '--port', metavar='port', type=int, default=7180, help="Cloudera Manager's port.")
+    parser.add_argument('-u', '--username', metavar='USERNAME', type=str, default='admin',
+                        help="The username to log into Cloudera Manager with.")
+    parser.add_argument('-pwd', '--password', metavar='PASSWORD', type=str, default='admin',
+                        help="The password to log into Cloudera Manager with.")
+    parser.add_argument('--use-tls', action='store_true', help="Whether to use TLS to connect to Cloudera Manager.")
+    parser.add_argument('--source_cm_url', metavar='Source Cloudera Manager URL', type=str, help="Full CM URL of the source cluster ie. https://hostname:7183/")
+    parser.add_argument("--source-user", metavar='Source Cloudera Manager Username', type=str, default='admin',
+                        help="The username to log into Source Cloudera Manager with." )
+    parser.add_argument("--source-password", metavar='SOURCE_CM_PWD', type=str, default='admin',
+                        help="The password to log into Source Cloudera Manager with." )
+    parser.add_argument("--peer-name" , metavar='PEER_NAME', type=str, default='peer1',
+                        help="ALias Name to be created of the Source cluster" )
+    return parser.parse_args()
+
+
+
 
 def get_service_name(SERVICE_TYPE, cluster_api, CLUSTER_NAME):
     """
@@ -15,6 +43,14 @@ def get_service_name(SERVICE_TYPE, cluster_api, CLUSTER_NAME):
             return service_name.name
 
 
+
+PEER_NAME='peer1'
+SOURCE_CLUSTER_NAME='sameer-testspot'
+SOURCE_HDFS_NAME='CD-HDFS-VHPVExTo'
+TARGET_CLUSTER_NAME='sameer-testspot-dest'
+TARGET_HDFS_NAME='CD-HDFS-KukHKtDK'
+TARGET_YARN_SERVICE='CD-YARN-rnMjblqZ'
+TARGET_CM_HOST="18.205.59.216"
 SOURCE_CM_HOST="34.226.244.149"
 TARGET_CM_HOST="18.205.59.216"
 TARGET_CLUSTER_NAME='sameer-testspot-dest'
@@ -32,29 +68,47 @@ print SOURCE_HDFS_NAME
 
 
 
+def main():
+    """
+    Add peer to the cluster.
+    @rtype:   number
+    @returns: A number representing the status of success.
+    """
+    settings = parse_args()
+    if len(sys.argv) == 1 or len(sys.argv) > 17:
+        print_usage_message()
+        quit(1)
 
-# PEER_NAME='peer1'
-# SOURCE_CLUSTER_NAME='sameer-testspot'
-# SOURCE_HDFS_NAME='CD-HDFS-VHPVExTo'
-# TARGET_CLUSTER_NAME='sameer-testspot-dest'
-# TARGET_HDFS_NAME='CD-HDFS-KukHKtDK'
-# TARGET_YARN_SERVICE='CD-YARN-rnMjblqZ'
-# TARGET_CM_HOST="18.205.59.216"
-#
-# api_root = ApiResource(TARGET_CM_HOST, username="admin", password="admin")
-# hdfs = api_root.get_cluster(TARGET_CLUSTER_NAME).get_service(TARGET_HDFS_NAME)
-#
-# hdfs_args = ApiHdfsReplicationArguments(None)
-# hdfs_args.sourceService = ApiServiceRef(None,
-#                                         peerName=PEER_NAME,
-#                                         clusterName=SOURCE_CLUSTER_NAME,
-#                                         serviceName=SOURCE_HDFS_NAME)
-# hdfs_args.sourcePath = '/'
-# hdfs_args.destinationPath = '/'
-# hdfs_args.mapreduceServiceName = TARGET_YARN_SERVICE
-#
-# # creating a schedule with daily frequency
-# start = datetime.datetime.now() # The time at which the scheduled activity is triggered for the first time.
-# end = start + datetime.timedelta(days=365) # The time after which the scheduled activity will no longer be triggered.
-#
-# schedule = hdfs.create_replication_schedule(start, end, "DAY", 1, True, hdfs_args)
+    api_target = ApiResource(settings.server, settings.port, settings.username,settings.password, settings.use_tls, 14)
+    cm_target = api_target.get_cloudera_manager()
+
+
+
+    hdfs = api_root.get_cluster(TARGET_CLUSTER_NAME).get_service(TARGET_HDFS_NAME)
+
+    hdfs_args = ApiHdfsReplicationArguments(None)
+    hdfs_args.sourceService = ApiServiceRef(None,
+                                        peerName=PEER_NAME,
+                                        clusterName=SOURCE_CLUSTER_NAME,
+                                        serviceName=SOURCE_HDFS_NAME)
+    hdfs_args.sourcePath = '/'
+    hdfs_args.destinationPath = '/'
+    hdfs_args.mapreduceServiceName = TARGET_YARN_SERVICE
+
+    # creating a schedule with daily frequency
+    start = datetime.datetime.now() # The time at which the scheduled activity is triggered for the first time.
+    end = start + datetime.timedelta(days=365) # The time after which the scheduled activity will no longer be triggered.
+
+    schedule = hdfs.create_replication_schedule(start, end, "DAY", 1, True, hdfs_args)
+
+
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+
+
+
+api_root = ApiResource(TARGET_CM_HOST, username="admin", password="admin")
