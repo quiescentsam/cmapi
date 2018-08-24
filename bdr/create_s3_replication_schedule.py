b4514e26dd89274e39a460c49a5ea71c964ea806
sam <sameerahmad@cloudera.com>
2018-08-24 13:06:01 -0400

    2018082413

--- bdr/create_s3_replication_schedule.py
+++ bdr/create_s3_replication_schedule.py
@@ -28,16 +28,13 @@ def parse_args():
     return parser.parse_args()
 
 def print_usage_message():
-    print ("usage: create_replication_schedule.py [-h] [-s HOST] [-p port] [-u USERNAME] \
-          [-pwd PASSWORD] [--use-tls] \
-          [--source-server Source Cloudera Manager URL] \
-          [--source-port Source port] \
-          [--source-user Source Cloudera Manager Username] \
-          [--source-password SOURCE_CM_PWD] \
-          [--s-use-tls] [--peer-name PEER_NAME] \
-          [-sp SOURCE PATH] [-tp DESTINATION PATH] \
-          [--source-cluster-name Source Cluster Name] \
-          [--target-cluster-name Destination Cluster Name]")
+    print ("usage: create_s3_replication_schedule.py [-h] [-s HOST] [-p port] \
+    [-u USERNAME] [-pwd PASSWORD]
+        [--use-tls]
+        [--account_name ACCOUNT_NAME]
+        [-sp SOURCE PATH]
+        [-tp DESTINATION PATH]
+        [--cluster-name Source Cluster Name]")
 
 
 
