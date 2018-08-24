25fca17cb68b957431332276c94fbcb82c31720e
sam <sameerahmad@cloudera.com>
2018-08-24 12:15:01 -0400

    2018082412

--- bdr/add_s3account.py
+++ bdr/add_s3account.py
@@ -7,16 +7,33 @@ import argparse
 import sys
 
 
-ACCESS_KEY="abcsfsg"
-SECRET_KEY="asfasdgafg"
-TYPE_NAME = 'AWS_ACCESS_KEY_AUTH'
-account_configs ={'aws_access_key': ACCESS_KEY,
-                  'aws_secret_key': SECRET_KEY}
-cm.api.create_external_account("cloudAccount1",
-                               "cloudAccount1",
-                               TYPE_NAME,
-                               account_configs=account_configs)
+def parse_args():
+    """
+    Parses host and cluster information from the given command line arguments.
+    @rtype:  namespace
+    @return: The parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="Adding Source cluster as 'peer' in Destination Cloudera Manager ",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-s', '--server', metavar='HOST', type=str, help="The Cloudera Manager host")
+    parser.add_argument('-p', '--port', metavar='port', type=int, default=7180, help="Cloudera Manager's port.")
+    parser.add_argument('-u', '--username', metavar='USERNAME', type=str, default='admin',
+                        help="The username to log into Cloudera Manager with.")
+    parser.add_argument('-pwd', '--password', metavar='PASSWORD', type=str, default='admin',
+                        help="The password to log into Cloudera Manager with.")
+    parser.add_argument('--use-tls', action='store_true', help="Whether to use TLS to connect to Cloudera Manager.")
+    parser.add_argument("--account-name" , metavar='ACCOUNT_NAME', type=str, default='cloudAccount1',
+                        help="ALias Name to be created of the Source cluster" )
+    parser.add_argument('-akey','--aws-access-key', metavar='AWS_ACCESS_KEY', type=str)
+    parser.add_argument('-skey','--aws-secret-key', metavar='AWS_SECRET_KEY', type=str)
+    return parser.parse_args()
 
+
+def print_usage_message():
+    print ("Usage: add_peer.py [-h] [-s HOST] [-p port] [-u USERNAME] [-pwd PASSWORD] \
+                                 [--use-tls] [--source_cm_url Source Cloudera Manager URL] \
+                                 [--source-user Source Cloudera Manager Username] \
+                                 [--source-password SOURCE_CM_PWD] [--peer-name PEER_NAME]")
 
 
 def main():
@@ -31,16 +48,12 @@ def main():
         quit(1)
 
     api_target = ApiResource(settings.server, settings.port, settings.username,settings.password, settings.use_tls, 14)
-    ACCESS_KEY="abcsfsg"
-    SECRET_KEY="asfasdgafg"
     TYPE_NAME = 'AWS_ACCESS_KEY_AUTH'
-    account_configs ={'aws_access_key': ACCESS_KEY,
-                  'aws_secret_key': SECRET_KEY}
-     api_target.create_external_account("cloudAccount1","cloudAccount1",
-                               TYPE_NAME,
-                               account_configs=account_configs)
-
-        print "Peer Successfully Added"
+    account_configs ={'aws_access_key': settings.aws_access_key,
+                  'aws_secret_key': settings.aws_secret_key}
+    try:
+        api_target.create_external_account(settings.account_name,settings.account_name,TYPE_NAME,account_configs=account_configs)
+        print "S3 Account Successfully Added"
     except ApiException as e:
         if 'already exists' in str(e):
             print 'Peer Already exists'
