4cd73194a04939345b9b2dd96ad09fb86a8d8b30
sam <sameerahmad@cloudera.com>
2018-09-02 14:24:01 -0400

    2018090214

--- bdr/create_s3_replication_schedule.py
+++ bdr/create_s3_replication_schedule.py
@@ -13,9 +13,7 @@ def parse_args():
     @rtype:  namespace
     @return: The parsed arguments.
     """
-    parser = argparse.ArgumentParser(description="Adding Source "
-                                                 "cluster as 'peer' in Destination Cloudera Manager ",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(description="Adding Source cluster as peer in Destination Cloudera Manager ", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-s', '--server', metavar='HOST', type=str,
                         help="The Cloudera Manager host")
     parser.add_argument('-p', '--port', metavar='port', type=int,
