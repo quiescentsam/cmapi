075b4cf2b7c09b4251c55d5de37f446d2319965a
sam <sameerahmad@cloudera.com>
2018-09-12 09:27:00 -0500

    2018091209

--- hdfs/usage_reports.sh
+++ hdfs/usage_reports.sh
@@ -1,1 +1,3 @@
 #!/usr/bin/env bash
+curl -v -k -X GET -u admin:pw 'https://tls.cluster.com:7183/api/v14/clusters/Cluster%201/services/ \
+hdfs1/reports/hdfsUsageReport?nameservice=nameservice1&aggregation=daily'
