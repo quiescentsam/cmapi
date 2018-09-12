f3bb45d499871f6933a5f08bdab94793c7c0ae2e
sam <sameerahmad@cloudera.com>
2018-09-12 09:33:01 -0500

    2018091209

--- hdfs/usage_reports.sh
+++ hdfs/usage_reports.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
-curl -v -k -X GET -u admin:admin 'http://nightly512-1.vpc.cloudera.com:7180/api/v14/clusters/Cluster%201/services/ \
-hdfs1/reports/hdfsUsageReport?nameservice=nameservice1&aggregation=daily'
+
+
+
+curl -v -k -X GET -u admin:admin 'http://nightly512-1.vpc.cloudera.com:7180/api/v14/clusters/Cluster%201/services'
+
+
+
+curl -v -k -X GET -u admin:admin 'http://nightly512-1.vpc.cloudera.com:7180/api/v14/clusters/Cluster%201/services/HDFS-1/reports/hdfsUsageReport?nameservice=nameservice1&aggregation=daily'
