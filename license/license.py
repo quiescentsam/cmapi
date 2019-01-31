75aa69f417d10c4ef2c2c0a1185b6a3283898fa2
sam <sameerahmad@cloudera.com>
2019-01-31 16:27:04 -0500

    2019013116

--- license/license.py
+++ license/license.py
@@ -1,7 +1,7 @@
 from cm_api.api_client import ApiResource
 from cm_api.endpoints.cms import ClouderaManager
 
-cm_host = "host00000"
+cm_host = "nightly515-1.vpc.cloudera.com"
 cm_port = 7180
 cm_username = 'admin'
 cm_password = 'admin'
@@ -9,4 +9,5 @@ cm_password = 'admin'
 
 api = ApiResource(cm_host, cm_port, cm_username, cm_password, version=7)
 cm = ClouderaManager(api)
-cm.get_license()
+license = cm.get_license()
+print license
