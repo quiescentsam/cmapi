9fd1c6c4e79f78467e6a02b06e3c326e6472bd99
sam <sameerahmad@cloudera.com>
2019-01-31 17:18:04 -0500

    2019013117

--- license/license.py
+++ license/license.py
@@ -7,8 +7,9 @@ cm_username = 'admin'
 cm_password = 'admin'
 license_file = '/Users/ssiddiqui/Desktop/SAMEER2.0/WORK/CODE/cloud/sameer_ahmad_siddiqui_cloudera_enterprise_license.txt'
 
-
 api = ApiResource(cm_host, cm_port, cm_username, cm_password, version=7)
 cm = ClouderaManager(api)
 license = open(license_file, 'r').read()
 cm.update_license(license)
+
+
