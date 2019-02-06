d919ede1185aafe04b77505223559be60e0c66a8
sam <sameerahmad@cloudera.com>
2019-02-06 14:42:03 -0500

    2019020614

---
+++ ha/sentry_ha.py
@@ -0,0 +1,10 @@
+from cm_api.api_client import ApiResource
+from cm_api.endpoints.cms import ClouderaManager
+
+cm_host = "sam-1.vpc.cloudera.com"
+cm_port = 7180
+cm_username = 'admin'
+cm_password = 'admin'
+
+api = ApiResource(cm_host, cm_port, cm_username, cm_password, version=15)
+
