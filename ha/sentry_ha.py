b4bedad38db632525b8edbd26ffb06a3e173d1b9
sam <sameerahmad@cloudera.com>
2019-02-06 15:18:03 -0500

    2019020615

--- ha/sentry_ha.py
+++ ha/sentry_ha.py
@@ -8,7 +8,9 @@ cm_password = 'admin'
 
 api = ApiResource(cm_host, cm_port, cm_username, cm_password, version=15)
 cluster = api.get_cluster()
-cluster.
+print cluster
+cluster.services.enable_rm_ha()
+cluster.services.enable_sentry_ha()
 
 
 
@@ -25,21 +27,21 @@ def enable_sentry_ha(self, new_sentry_host_id, new_sentry_role_name, zk_service_
     )
     return self._cmd('enableSentryHa', data=args)
 
-def enable_rm_ha(self, new_rm_host_id, zk_service_name=None):
-    """
-    Enable high availability for a YARN ResourceManager.
-    @param new_rm_host_id: id of the host where the second ResourceManager
-                           will be added.
-    @param zk_service_name: Name of the ZooKeeper service to use for auto-failover.
-           If YARN service depends on a ZooKeeper service then that ZooKeeper
-           service will be used for auto-failover and in that case this parameter
-           can be omitted.
-    @return: Reference to the submitted command.
-    @since: API v6
-    """
-    args = dict(
-        newRmHostId = new_rm_host_id,
-        zkServiceName = zk_service_name
-    )
-    return self._cmd('enableRmHa', data=args)
+# def enable_rm_ha(self, new_rm_host_id, zk_service_name=None):
+#     """
+#     Enable high availability for a YARN ResourceManager.
+#     @param new_rm_host_id: id of the host where the second ResourceManager
+#                            will be added.
+#     @param zk_service_name: Name of the ZooKeeper service to use for auto-failover.
+#            If YARN service depends on a ZooKeeper service then that ZooKeeper
+#            service will be used for auto-failover and in that case this parameter
+#            can be omitted.
+#     @return: Reference to the submitted command.
+#     @since: API v6
+#     """
+#     args = dict(
+#         newRmHostId = new_rm_host_id,
+#         zkServiceName = zk_service_name
+#     )
+#     return self._cmd('enableRmHa', data=args)
 
