4841095c6a81506f84b9f13391bd43b379dbf813
sam <sameerahmad@cloudera.com>
2019-02-07 12:33:03 -0500

    2019020712

--- ha/sentry_ha.py
+++ ha/sentry_ha.py
@@ -46,7 +46,9 @@ if __name__ == '__main__':
             "slaveFailCountThreshold": 10
         }
     }
-    headers = {'Content-Type': 'application/json'}
+    headers = {
+        'Content-Type': 'application/json',
+    }
 
     #enable = requests.post("http://bluedata-gsk-1.vpc.cloudera.com:7180/api/v19/clusters/cluster_1/services/sentry/commands/enableSentryHa", auth=('admin', 'admin'), data=arguments, headers=headers)
     enable = requests.post("http://bluedata-gsk-1.vpc.cloudera.com:7180/api/v19/clusters/cluster_1/services/sentry/commands/enableSentryHa", auth=('admin', 'admin'), data=arguments, headers=headers)
